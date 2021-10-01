6a109844e9b15bb4de63ce72b2567140408cd06e
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-10-01 14:30:23 +0700

    feature: excercise lab02

--- Lab01/exercise-3-check-uri-exist.php
+++ Lab01/exercise-3-check-uri-exist.php
@@ -1,13 +1,18 @@
 <html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php 
-        if (file_exists("test.txt")) {
-            echo file_get_contents("test.txt");
-        }
-    ?> 
-    <a href="https://ctt.hust.edu.vn/">HUST UNIVERSITY</a>
-  </body>
+
+<head>
+  <title>PHP Test</title>
+</head>
+
+<body>
+  <?php
+  if (file_exists("test.txt")) {
+    echo file_get_contents("test.txt");
+  } else {
+    echo "File not exist";
+  }
+  ?>
+  <a href="https://ctt.hust.edu.vn/">HUST UNIVERSITY</a>
+</body>
+
 </html>
